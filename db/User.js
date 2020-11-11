da79071eeeaac46374bdf3518206cc8935f4a9d3
Mike Zahuta <mike.zahuta@gmail.com>
2020-11-11 12:31:39 -0500

    initial User.js model and installations

---
+++ db/User.js
@@ -0,0 +1,27 @@
+const { Schema } = require('mongoose')
+
+module.exports = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password_digest: {
+            type: String,
+            required: true
+        },
+        trips: [{
+            type: Schema.Types.ObjectId,
+            ref: 'trips'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'users'
+        }]
+    },
+    {timestamps: true}
+)
